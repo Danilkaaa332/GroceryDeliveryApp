cfbc52aba101b3440dc6f3025abb007ff68d65d8
Danilkaaa332 <drizzlex121@gmail.com>
2026-05-18 11:36:34 +0300

    Init

---
+++ GroceryDeliveryApp.Core/DeliveryOrder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryDeliveryApp.Core
+{
+    public class DeliveryOrder
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Пользователь
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// Адрес
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// Список продуктов
+        /// </summary>
+        public List<GroceryProduct> Products { get; set; }
+        /// <summary>
+        /// Стоимость доставки
+        /// </summary>
+        public decimal DeliveryPrice { get; set; }
+        /// <summary>
+        /// Расчет стоимости корзины
+        /// </summary>
+        /// <returns></returns>
+        public decimal CalculateBasketPrice()
+        {
+            return Products.Sum(x => x.Price * x.Quantity);
+        }
+        /// <summary>
+        /// Формирование итогового заказа
+        /// </summary>
+        /// <returns></returns>
+        public decimal GetTotalPrice()
+        {
+            return CalculateBasketPrice() + DeliveryPrice;
+        }
+        /// <summary>
+        /// Расчет стоимости доставки
+        /// </summary>
+        /// <param name="basketPrice"></param>
+        /// <returns></returns>
+        public decimal CalculateDeliveryPrice(decimal basketPrice)
+        {
+            if (basketPrice > 1000) return 0;
+            else return 250;
+        }
+    }
+}
---
+++ GroceryDeliveryApp.Core/GroceryProduct.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace GroceryDeliveryApp.Core
+{
+    /// <summary>
+    /// Продукт магазина
+    /// </summary>
+    public class GroceryProduct
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название продукта
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Категория
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// Цена
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Количество
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Срок годности
+        /// </summary>
+        public DateTime ExprirationDate { get; set; }
+
+        /// <summary>
+        /// Проверка срока годности
+        /// </summary>
+        public bool IsExpired()
+        {
+            return ExprirationDate < DateTime.Now;
+        }
+    }
+}
